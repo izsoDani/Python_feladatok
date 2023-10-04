d1ad6d6779dc9ae51775b8f16cc8e6f53c881cec
Izsó Dániel <p-daniel.izso@smp.hu>
2023-10-04 15:27:44 +0200

    2010majus

---
+++ 2010majus.py
@@ -0,0 +1,40 @@
+ulohelyek = []
+
+fajl = open("2010majus.txt", "r")
+for sor in fajl:
+    adatok = sor.strip().split()
+    ulohelyek.append(adatok)
+fajl.close()
+print(ulohelyek)
+
+szemelyes = []
+for adat in ulohelyek:
+    mindenadat = {}
+    mindenadat['ulohely'] = adat[0]
+    mindenadat['felszall'] = adat[1]
+    mindenadat['leszall'] = adat[2]
+    szemelyes.append(mindenadat)
+print(szemelyes)
+
+fontos_adatok = []
+elso_harom_adat = szemelyes[:1]
+for elsoadatok in elso_harom_adat:
+    alap_adatok = {}
+    alap_adatok['elkelt'] = elsoadatok['ulohely']
+    alap_adatok['uthossz'] = elsoadatok['felszall']
+    alap_adatok['fizetendo'] = elsoadatok['leszall']
+    fontos_adatok.append(alap_adatok)
+
+print(fontos_adatok)
+
+print("2.feladat")
+
+utolso_adat = szemelyes[-1]
+print("Legutolsó vásárló adatai, Sorszám:",utolso_adat['ulohely'],"Tavolsg:",(int(utolso_adat['leszall'])-int(utolso_adat['felszall'])),"km")
+
+print("3.feladat")
+
+for teljes in szemelyes:
+    if int(teljes['leszall'])-int(teljes['felszall']) == 172:
+        print(teljes['ulohely'])
+
